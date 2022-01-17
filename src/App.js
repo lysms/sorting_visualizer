22dcffe846fe569b0a05f1f059012e4532a71012
Yanshen <yanshenlin1001@gmail.com>
2022-01-17 16:12:09 -0500

    project setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+
+
+
+import Visualizer from './Visualizer/Visualizer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Sorting Visualizer Builder</h1>
+        <Visualizer />
       </header>
     </div>
   );
---
+++ src/Visualizer/Visualizer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Visualizer.css"
+
+export default class Visualizer extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>This is the visualizer section</p>
+            </div>
+        )
+    }
+}
